c9acbeeeb20a563c08920cae41555f85e0adfa00
Ashish <ashish@pratilipi.com>
2023-02-08 13:24:51 +0530

    REFACTORED dpk.js

--- dpk.js
+++ dpk.js
@@ -1,28 +1,28 @@
 const crypto = require("crypto");
 
+const createHash = (data) => crypto.createHash("sha3-512").update(data).digest("hex");;
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate=TRIVIAL_PARTITION_KEY;
 
-  if (event) {
-    if (event.partitionKey) {
+  if (!event)
+    return candidate
+
+  if (event.partitionKey) {
+    if (typeof event.partitionKey !== "string") {
+      candidate = JSON.stringify(event.partitionKey);
+    }else{
       candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
     }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
+    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+      candidate = createHash(candidate);
     }
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    candidate = createHash(data);
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
+  
   return candidate;
 };
